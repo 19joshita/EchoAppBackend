ef909f64579817155921b2d869a4a45901f9c1aa
19joshita <pateljoshita236@gmail.com>
2024-04-23 16:51:11 +0530

    end-code

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -36,3 +36,67 @@ exports.getAllPosts = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+exports.getUserPosts = async (req, res) => {
+  try {
+    const UserPost = await postModel
+      .find({ postedBy: req?.user?.userId })
+      .populate("postedBy", "_id, name")
+      .sort({ createdAt: -1 });
+    res
+      .status(200)
+      .json({ message: "Posts fetched successfully", status: true, UserPost });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+exports.deletePostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedPost = await postModel.findByIdAndDelete(id);
+    if (!deletedPost) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Post deleted successfully", status: true });
+  } catch (error) {
+    console.error("Error deleting post by ID:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+exports.getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await postModel.findById(id).populate("postedBy", "username");
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Post found successfully", status: true, post });
+  } catch (error) {
+    console.error("Error getting post by ID:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+// Controller function to update a post by ID
+exports.updatePostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    const updatedPost = await postModel.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+    if (!updatedPost) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Post updated successfully", post: updatedPost });
+  } catch (error) {
+    console.error("Error updating post by ID:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -9,5 +9,13 @@ router.post(
   postController.createPostController
 );
 router.get("/allPost", jsonAuthMiddleware, postController.getAllPosts);
+router.get("/user-post", jsonAuthMiddleware, postController.getUserPosts);
+router.delete(
+  "/delete-post/:id",
+  jsonAuthMiddleware,
+  postController.deletePostById
+);
+router.get("/post/:id", jsonAuthMiddleware, postController.getPostById);
+router.put("/post/:id", jsonAuthMiddleware, postController.updatePostById);
 
 module.exports = router;
